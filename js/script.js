53231c96cad29870ba55e595200ad7531ce24bbc
Osman Can <111634974+OsmnCnG@users.noreply.github.com>
2023-07-02 17:58:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -102,6 +102,8 @@ function buildAndShowHomeHTML (categories) {
       var chosenCategoryShortName = choosenCategory.short_name;
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
 
+
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
